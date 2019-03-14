42c755bf2348c41eb6e17a06c887181b0e28f6d2
gabe3 <Dinya7991>
2019-03-14 00:17:14 -0400

    Task: Changed content

--- web/views/layout.php
+++ web/views/layout.php
@@ -5,7 +5,11 @@
 </head>
 
 <body>
-    <h1>Hello</h1>
+<?php
+
+    include "C:\Users\gabe3\PhpstormProjects\IS218HerokuProject1\IS218Practice\src\index.php";
+    index.php();
+?>
 
 </body>
 </html>
